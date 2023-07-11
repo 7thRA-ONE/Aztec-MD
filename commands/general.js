c4a98fc71c20e935574743bd770d1a02e00f1c2c
vorterxi <139119215+vorterxi@users.noreply.github.com>
2023-07-11 14:45:52 +0200

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -4,15 +4,7 @@
    category: "Mics",
    description: 'Some virous commands in one',
    start: async(client, m, { prefix, botName, toReact }) => {
-   switch (inputCMD) {
-
-     case 'alive':
-     await toReact('🔥');
        let msg = `Hey ${pushName} This is ${botName} developed by Diegoson\n\n*👾Descripto: Whats script bot\n*🚦Botname: ${botName}\n*👾Prefix: ${prefix}\n*📲Version: 3.0.0\n\n\n*©️VORTERX`;
-       client.sendMessage(m.from, { image: pic, caption: msg }, { quoted: from});
- break;
- default;
- break;
-     }
-   },
- };
+     await client.sendMessage(m.from, { image: pic, caption: msg }, { quoted: from});
+        }
+ 

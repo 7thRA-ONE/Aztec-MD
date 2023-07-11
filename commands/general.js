5196d16b10ca996627bf1db56eb693aa1399a9dd
vorterxi <139119215+vorterxi@users.noreply.github.com>
2023-07-11 14:51:01 +0200

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -1,7 +1,7 @@
  module.exports = {
    name: 'alive',
-   alias: ["alv"],
-   category: "Mics",
+   alias: ['alv'],
+   category: 'Mics',
    description: 'Some virous commands in one',
    start: async(client, m, { prefix, botName, toReact }) => {
        let msg = `Hey ${pushName} This is ${botName} developed by Diegoson\n\n*👾Descripto: Whats script bot\n*🚦Botname: ${botName}\n*👾Prefix: ${prefix}\n*📲Version: 3.0.0\n\n\n*©️VORTERX`;

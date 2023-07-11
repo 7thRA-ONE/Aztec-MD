3f932c9eeb2d296cb3f96f1fadae1f8ac8cd84a5
vorterxi <139119215+vorterxi@users.noreply.github.com>
2023-07-11 14:46:53 +0200

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -5,6 +5,6 @@
    description: 'Some virous commands in one',
    start: async(client, m, { prefix, botName, toReact }) => {
        let msg = `Hey ${pushName} This is ${botName} developed by Diegoson\n\n*👾Descripto: Whats script bot\n*🚦Botname: ${botName}\n*👾Prefix: ${prefix}\n*📲Version: 3.0.0\n\n\n*©️VORTERX`;
-     await client.sendMessage(m.from, { image: pic, caption: msg }, { quoted: from});
+     await client.sendMessage(m.from, { image: pic, caption: msg }, { quoted: m});
         }
- 
+      }

4b10e54466ee39e81336026212a50ef9263f43be
vorterxi <139119215+vorterxi@users.noreply.github.com>
2023-07-12 10:46:35 +0200

    Update core.js

--- core.js
+++ core.js
@@ -316,6 +316,11 @@ const connect = async () => {
 };
 
 connect();
+app.use("/", 
+     
+  res.setHeader("content-type", "image/png");
+  res.send(await qrcode.toBuffer(QR_GENERATE));
+});
 app.listen(PORT, () => {
   console.log(`This Server is running on PORT ${PORT}`);
 });

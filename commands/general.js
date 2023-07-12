1b8bbcc2a0acf47523aada73c153cf2b9976a1a0
vorterxi <139119215+vorterxi@users.noreply.github.com>
2023-07-12 09:57:14 +0200

    vorterx

--- commands/general.js
+++ commands/general.js
@@ -1,10 +1,17 @@
  module.exports = {
-   name: 'alive',
+   name: 'both',
    alias: ['alv'],
    category: 'Mics',
    description: 'Some virous commands in one',
-   start: async(client, m, { prefix, botName, toReact }) => {
+   start: async(vorterx, m, { prefix, botName, toReact }) => {
+
+    case "alive":
+    
        let msg = `Hey ${pushName} This is ${botName} developed by Diegoson\n\n*👾Descripto: Whats script bot\n*🚦Botname: ${botName}\n*👾Prefix: ${prefix}\n*📲Version: 3.0.0\n\n\n*©️VORTERX`;
-     await client.sendMessage(m.from, { image: pic, caption: msg }, { quoted: m});
-        }
-      }
+     await vorterx.sendMessage(m.from, { image: pic, caption: msg }, { quoted: m});
+        break;
+      default:
+        break;
+    }
+  },
+};

59c08c2df2f38e46662b2f61d7e45dad9a78debd
vorterxi <139119215+vorterxi@users.noreply.github.com>
2023-07-12 10:00:02 +0200

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -8,10 +8,9 @@
     case 'alive':
       let msg = `Hey ${pushName} This is ${botName} developed by Diegoson\n\n*👾Descripto: Whats script bot\n*🚦Botname: ${botName}\n*👾Prefix: ${prefix}\n*📲Version: 3.0.0\n\n\n*©️VORTERX`;
      await vorterx.sendMessage(m.from, { image: pic, caption: msg }, { quoted: m});
-   }
+
         break;
       default:
         break;
     }
-  },
-};
+  };

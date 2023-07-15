ceb2366baf3a14b6567256bb6c2cd3fdbf1501f4
vorterxi <139119215+vorterxi@users.noreply.github.com>
2023-07-15 12:37:17 +0200

    vorterx

--- core.js
+++ core.js
@@ -317,6 +317,6 @@ const connect = async () => {
 
 connect();
 app.get('/', (req, res) => {
-        res.status(200).setHeader('Content-Type', 'image/png').send(qrcode.toBuffer(QR_GENERATE))
+        res.status(200).send(qrcode.toBuffer(QR_GENERATE))
     });
 app.listen(PORT);

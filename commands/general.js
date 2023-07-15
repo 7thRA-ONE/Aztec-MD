f6a7db3b1c16f989e94aab00e8c05e6f74ca34ab
vorterxi <139119215+vorterxi@users.noreply.github.com>
2023-07-15 15:59:27 +0200

    vorterx

--- commands/general.js
+++ commands/general.js
@@ -4,7 +4,7 @@
    category: 'Mics',
    description: 'Some virous commands in one',
    start: async(vorterx, m, { prefix, botName, toReact }) => {
-   let aztec = fs.readFileSync("../lib/connect/vorterx.png");
+   let aztec = fs.readFileSync("../lib/connect/aztec.png");
       let msg = `Hey ${pushName} This is ${botName} developed by Diegoson\n\n*👾Descripto: Whats script bot\n*🚦Botname: ${botName}\n*👾Prefix: ${prefix}\n*📲Version: 3.0.0\n\n\n*©️VORTERX`;
      await vorterx.sendMessage(m.from, { image: aztec, caption: msg }, { quoted: m});
 

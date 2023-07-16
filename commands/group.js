9e1091a87c3fc8c0011ff8058bdb0b5e8febf8b8
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-16 16:37:17 +0200

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -1,19 +1,19 @@
-require ('../../../config')
+require ('../../../config');
 module.exports = {
 	name: "kick",
 	alias: ["remove"],
 	description: "remove Member from group",
 	category: "Group",
 	start: async(vorterx, m, { text, prefix, isBotAdmin,isAdmin,mentionByTag}) => {
-		if(!isAdmin) { react("⭕"); return m.reply(`*🔌This is admin only command*`);
+		if(!isAdmin) { await react("⭕"); return m.reply(`*🔌This is admin only command*`);
                  }
-		if(!isBotAdmin) { react("😭"); return m.reply(`*🔌I need to be an admin in order to use this command*`);
+		if(!isBotAdmin) { await react("😭"); return m.reply(`*🔌I need to be an admin in order to use this command*`);
                     }
 		const mention= await mentionByTag
-		if(!isAdmin) { react("⭕"); return m.reply(`*🔌This is admin only command*`);
+		if(!isAdmin) { await react("⭕"); return m.reply(`*🔌This is admin only command*`);
   }
 		let users = await (mention[0]) || m.msg.contextInfo.participant
-		if (!users) { react("❌"); return m.reply(`*🤔No user ld found*`);
+		if (!users) { await react("❌"); return m.reply(`*🤔No user ld found*`);
                 }
                react("🎊");
 			await vorterx.groupParticipantsUpdate(m.from, [users], "remove")

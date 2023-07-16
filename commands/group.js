81b32ef8c778da060f5feadb9fadaba97a3dfe25
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-16 20:22:42 +0200

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -7,18 +7,18 @@ description: "remove Member from group",
 category: "Group",
 start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, mentionByTag, pushName }) => {
 if (!isAdmin) {
-await toReact("⭕"); return m.reply(*🔌This command is for admin only*);
+await toReact("⭕"); return m.reply("*🔌This command is for admin only*");
 }
 if (!isBotAdmin) {
-await toReact("😭"); return m.reply(*🔌I need to be an admin in order to use this command*);
+await toReact("😭"); return m.reply("*🔌I need to be an admin in order to use this command*");
 }
 const mention = await mentionByTag
 if (!mention[0]) {
-await toReact("❌"); return m.reply(*🤔No user found*);
+await toReact("❌"); return m.reply("*🤔No user found*");
 }
 await toReact("🎊");
 await vorterx.groupParticipantsUpdate(m.from, [mention[0]], "remove")
-await vorterx.sendMessage(m.from, { text: *🎊User has been removed by ${pushName}* }, { quoted: m })
+await vorterx.sendMessage(m.from, { text: `*🎊User has been removed by ${pushName}`* }, { quoted: m })
 },
 }
 

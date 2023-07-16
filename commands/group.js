d2f8d0d1b2e00d621a2870c8a7b290e10601693e
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-16 20:12:14 +0200

    vorterx

--- commands/group.js
+++ commands/group.js
@@ -64,46 +64,38 @@ module.exports = {
 //--------------------------------------------------------------------------
 
 module.exports = {
-  name: "tagall",
-  alias: ["taga"],
-  description: "tag members",
-  category: "Group",
-  start: async (vorterx, m, { text, prefix, isBotAdmin, isAdmin }) => {
-    if (!isAdmin) {
-      await toReact("⭕");
-      return m.reply(`*🔌This is admin only command*`);
-    }
-    if (!isBotAdmin) {
-      await toReact("😭");
-      return m.reply(`*🔌I need to be an admin in order to use this command*`);
-    }
-    const participants = await vorterx.groupMetadata(m.chat).participants;
-    if (!participants) {
-      await toReact("❌");
-      return m.reply(`*🤔No user found*`);
-    }
-    if (!isMedia) {
-      var message2 = m.quoted ? m.quoted.msg : text || "No message";
-    } else {
-      message2 = "Check this Out!";
-    }
+    name: "tagall",
+    alias: ["taga"],
+    description: "tag members",
+    category: "Group",
+    start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, isMedia, participants }) => {
+        if (!isAdmin) {
+            await toReact("⭕");
+            return m.reply(`*🔌This is admin only command*`);
+        }
+        if (!isBotAdmin) {
+            await toReact("😭");
+            return m.reply(`*🔌I need to be an admin in order to use this command*`);
+        }
+        if (!isMedia) {
+            var message2 = m.quoted
+                ? m.quoted.msg
+                : text || "No message";
+        } else {
+            message2 = "Check this Out!";
+        }
 
-    let mess = `『 *Attention Everybody* 』
-    
-*⚜️ Tagged by:* @${m.sender.split("@")[0]}
-            
-*🧩 Message:* ${message2}
-│\n`;
-    for (let mem of participants) {
-      mess += `┟ @${mem.id.split("@")[0]}\n`;
-    }
-    mess += `╰────────────⊰\n\n                    *Thank You*\n`;
+        let mess = `❯`;
+        for (let mem of participants) {
+            mess += `❯ @${mem.id.split("@")[0]}\n`;
+        }
+        mess += `╰────────────⊰\n\n`;
 
-    await toReact("〽️");
-    vorterx.sendMessage(
-      m.from,
-      { text: mess, mentions: participants.map((a) => a.id) },
-      { quoted: m }
-    );
-  },
+        await toReact("💘");
+        vorterx.sendMessage(
+            m.from,
+            { text: mess, mentions: participants.map((a) => a.id) },
+            { quoted: m }
+        );
+    },
 };

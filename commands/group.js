fd2c28215f48c2a6387cdb80605da6b4a2d7c2ff
HyNO <111696936+HyNO-IR@users.noreply.github.com>
2023-07-16 21:57:49 +0330

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -1,93 +1,114 @@
 const config = require("../config");
 
 module.exports = {
-name: "kick",
-alias: ["remove"],
-description: "remove Member from group",
-category: "Group",
-start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, mentionByTag, pushName }) => {
-if (!isAdmin) {
-await toReact("⭕"); return m.reply("*🔌This command is for admin only*");
-}
-if (!isBotAdmin) {
-await toReact("😭"); return m.reply("*🔌I need to be an admin in order to use this command*");
-}
-const mention = await mentionByTag
-if (!mention[0]) {
-await toReact("❌"); return m.reply("*🤔No user found*");
-}
-await toReact("🎊");
-await vorterx.groupParticipantsUpdate(m.from, [mention[0]], "remove")
-await vorterx.sendMessage(m.from, { text: `*🎊User has been removed by ${pushName}`* }, { quoted: m })
-},
-}
-
-//--------------------------------------------------------------------------
-
-module.exports = {
-    name: "group",
-    description: "To close the group",
-    category: "Group",
-    start: async (vorterx, m, { text, prefix, isBotAdmin, isAdmin, args, pushName, toReact }) => {
-        if (!isAdmin) {
-            await toReact("🔇");
-            return m.reply("*🔇This command can only be used by Admin*");
-        }
-        if (!isBotAdmin) {
-            await toReact("🔇");
-            return m.reply("*😥I need to be admin inorder to use this command*");
-        }
-
-        if (args[0] === 'open') {
-            await toReact("🕳️");
-            await vorterx.groupSettingUpdate(m.from, 'not_announcement').then((res) => m.reply(`*🔇Group Has Been Opened By ${pushName}*`)).catch((err) => m.reply(jsonformat(err)), { quoted: m });
-        } else if (args[0] === 'close') {
-            await toReact("💣");
-            await vorterx.groupSettingUpdate(m.from, 'announcement').then((res) => m.reply(`*🔇Group Has been Closed By ${pushName}*`)).catch((err) => m.reply(jsonformat(err)), { quoted: m });
-        }
+  name: "kick",
+  alias: ["remove"],
+  description: "Remove member from group",
+  category: "Group",
+  start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, mentionByTag, pushName }) => {
+    if (!isAdmin) {
+      await toReact("⭕"); 
+      return m.reply(`*🔌 This command is for admin only*`); 
     }
+    
+    if (!isBotAdmin) {
+      await toReact("😭");
+      return m.reply(`*🔌 I need to be an admin in order to use this command*`);
+    }
+    
+    const mention = mentionByTag;
+    
+    if (!mention[0]) {
+      await toReact("❌");
+      return m.reply(`*🤔 No user found*`);
+    }
+    
+    await toReact("🎊");
+    
+    try {
+      await vorterx.groupParticipantsUpdate(m.from, [mention[0]], "remove");
+      await vorterx.sendMessage(m.from, { text: `*🎊 User has been removed by ${pushName}*` }, { quoted: m });
+    } catch (err) {
+      console.error(err);
+      await m.reply(`Failed to remove user: ${err}`); 
+    }
+  },
 };
 
-//--------------------------------------------------------------------------
+module.exports = {
+  name: "group",
+  description: "To close/open the group", 
+  category: "Group",
+  start: async (vorterx, m, { text, prefix, isBotAdmin, isAdmin, args, pushName, toReact }) => {
+    if (!isAdmin) {
+      await toReact("🔇");
+      return m.reply(`*🔇 This command can only be used by Admin*`);
+    }
+    
+    if (!isBotAdmin) {
+      await toReact("🔇");
+      return m.reply(`*😥 I need to be admin in order to use this command*`);
+    }
+
+    if (args[0] === "open") {
+      await toReact("🕳️");
+      try {
+        await vorterx.groupSettingUpdate(m.from, "not_announcement");
+        await m.reply(`*🔇 Group has been opened by ${pushName}*`);  
+      } catch (err) {
+        console.error(err);
+        await m.reply(`Failed to open group: ${err}`);
+      }
+    } else if (args[0] === "close") {
+      await toReact("💣");
+      try {
+        await vorterx.groupSettingUpdate(m.from, "announcement");
+        await m.reply(`*🔇 Group has been closed by ${pushName}*`);
+      } catch (err) {
+        console.error(err);
+        await m.reply(`Failed to close group: ${err}`);
+      }
+    }
+  }
+};
 
 module.exports = {
-    name: "tagall",
-    alias: ["taga"],
-    description: "tag members",
-    category: "Group",
-    start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, isMedia, participants }) => {
-        if (!isAdmin) {
-            await toReact("⭕");
-            return m.reply(`*🔌This is admin only command*`);
-        }
-        if (!isBotAdmin) {
-            await toReact("😭");
-            return m.reply(`*🔌I need to be an admin in order to use this command*`);
-        }
-        if (!isMedia) {
-            var message2 = m.quoted
-                ? m.quoted.msg
-                : text || "No message";
-        } else {
-            message2 = "Check this Out!";
-        }
-
-        let mess = `『 *Attention Everybody* 』
+  name: "tagall",
+  alias: ["taga"],
+  description: "Tag all members",
+  category: "Group",
+  start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, isMedia, participants }) => {
+    if (!isAdmin) {
+      await toReact("⭕");
+      return m.reply(`*🔌 This is admin only command*`);
+    }
     
-*⚜️ Tagged by:* @${m.sender.split("@")[0]}
-            
-*🧩 Message:* ${message2}
-│\n`;
-        for (let mem of participants) {
-            mess += `┟ @${mem.id.split("@")[0]}\n`;
-        }
-        mess += `╰────────────⊰\n\n                    *Thank You*\n`;
+    if (!isBotAdmin) { 
+      await toReact("😭");
+      return m.reply(`*🔌 I need to be an admin in order to use this command*`);
+    }
 
-        await toReact("〽️");
-        vorterx.sendMessage(
-            m.from,
-            { text: mess, mentions: participants.map((a) => a.id) },
-            { quoted: m }
-        );
-    },
+    let message2 = isMedia ? "Check this out!" : m.quoted ? m.quoted.msg : text || "No message";
+
+    let mess = `『 *Attention Everybody* 』\n\n*⚜️ Tagged by:* @${m.sender.split("@")[0]}\n\n*🧩 Message:* ${message2}\n`;
+    
+    for (let mem of participants) {
+      mess += `┟ @${mem.id.split("@")[0]}\n`; 
+    }
+    
+    mess += `╰────────────⊰\n\n                    *Thank You*\n`; 
+
+    try {
+      await toReact("〽️");
+      await vorterx.sendMessage(m.from, { 
+        text: mess,
+        mentions: participants.map(a => a.id)
+      }, {
+        quoted: m
+      });
+    } catch (err) {
+      console.error(err);
+      await m.reply(`Failed to tag all members: ${err}`);
+    }
+  },
 };

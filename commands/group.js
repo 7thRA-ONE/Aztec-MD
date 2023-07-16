82be16a532a7965cb2cec312362f5ac5706b5db9
HyNO <111696936+HyNO-IR@users.noreply.github.com>
2023-07-16 22:07:41 +0330

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -1,47 +1,50 @@
 const config = require("../config");
- async function toReact(emoji) {
-      let reactm = {
-        react: {
-          text: emoji,
-          key: m.key,
-        },
-      };
-      await vorterx.sendMessage(m.from, reactm);
+
+async function toReact(vorterx, m, emoji) {
+  let reactm = {
+    react: {
+      text: emoji,
+      key: m.key,
     }
+  };
+  
+  await vorterx.sendMessage(m.from, reactm);
+}
 
 module.exports = {
   name: "kick",
-  alias: ["remove"],
+  alias: ["remove"], 
   description: "Remove member from group",
   category: "Group",
-  start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, mentionByTag, pushName }) => {
-    if (!isAdmin) {
-      await toReact("⭕"); 
-      return m.reply(`*🔌 This command is for admin only*`); 
+  start: async (vorterx, m, { text, prefix, isBotAdmin, isAdmin, mentionByTag, pushName }) => {
+    if(!isAdmin) {
+      await toReact(vorterx, m, "⭕");
+      return m.reply(`*🔌 This command is for admin only*`);
     }
     
-    if (!isBotAdmin) {
-      await toReact("😭");
-      return m.reply(`*🔌 I need to be an admin in order to use this command*`);
+    if(!isBotAdmin) {
+      await toReact(vorterx, m, "😭");  
+      return m.reply(`*🔌 I need to be an admin to use this command*`);
     }
     
     const mention = mentionByTag;
-    
-    if (!mention[0]) {
-      await toReact("❌");
-      return m.reply(`*🤔 No user found*`);
+    if(!mention[0]) {
+      await toReact(vorterx, m, "❌");
+      return m.reply(`*🤔 No user found*`);  
     }
     
-    await toReact("🎊");
+    await toReact(vorterx, m, "🎊");
     
     try {
       await vorterx.groupParticipantsUpdate(m.from, [mention[0]], "remove");
-      await vorterx.sendMessage(m.from, { text: `*🎊 User has been removed by ${pushName}*` }, { quoted: m });
-    } catch (err) {
+      await vorterx.sendMessage(m.from, { 
+        text: `*🎊 User has been removed by ${pushName}*`,
+      }, {quoted: m});
+    } catch(err) {
       console.error(err);
-      await m.reply(`Failed to remove user: ${err}`); 
+      await m.reply(`Failed to remove user: ${err}`);
     }
-  },
+  }
 };
 
 module.exports = {

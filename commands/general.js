fe5f67632a19c9253f90a5f10c8658045b963072
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-16 21:49:48 +0200

    vorterx

--- commands/general.js
+++ commands/general.js
@@ -4,10 +4,11 @@ module.exports = {
    name: 'alive',
    alias: ['alv'],
    category: 'Mics',
-   description: 'Some virous commands in one',
+   description: 'An cmd to test if its on',
    start: async(vorterx, m, { prefix, pushName, botName }) => {
+      await toReact("💗");
    let aztec = fs.readFileSync("./lib/connect/vorterx.png");
-      let msg = `Hey ${pushName} This is ${botName} developed by Diegoson\n\n*👾Descripto: Whats script bot\n*🚦Botname: ${botName}\n*👾Prefix: ${prefix}\n*📲Version: 3.0.0\n\n\n*©️AZTEC-MD BY VORTERX*`;
+      let msg = `*Hey ${pushName} This is ${botName} developed by Diegoson*\n\n*👾Descripto*: Whats script bot by vorterx\n*🚦Botname*: AZTEC-MD\n*👾Prefix*: ${prefix}\n*📲Version*: 3.0.0\n\n\n*©️AZTEC-MD BY VORTERX*`;
      await vorterx.sendMessage(m.from, { image: aztec, caption: msg }, { quoted: m});
 
    }

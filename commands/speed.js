64dd06e5078259888ece1205b185f3b282d066b5
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-16 21:16:31 +0200

    Create speed.js

---
+++ commands/speed.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "ping",
+  description: "TO chek bot speeed",
+  category: "General",
+  start: async(vorterx, m, { prefix }) => {
+  await toReact("🏇");
+    
+  var new = new Date().getTime();
+  const {amarok} = vorterx.sendMessage(m.from, {text: "*Responding to speed"*});
+    var end = new Date().getTime();
+    return await vorterx.sendMessage(m.from,{text: "*Response* "+ (end - new) *", m:amarok});

0a6ca9c83cd70fc1a9328c13fc30b798e4f123d0
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-17 16:24:47 +0200

    v3.0.0

--- commands/leave.js
+++ commands/leave.js
@@ -4,6 +4,6 @@ module.exports = {
   category: "Group",
   start: async(vorterx, m, { isAdmin, isGroup, toReact, isBotAdmin}) => {
 
-  if(isAdmim){ await toReact("❌"); return m.reply("*📢 Sorry this cmd is for my owner only*");} if(iGroup) { await toReact("❌"); return m.reply("*🤔Weres you heading cz this is group cmd*");} return await vorterx.groupLeave(m.from) { quoted: m })
+  if(isAdmim){ await toReact("❌"); return m.reply("*📢 Sorry this cmd is for my owner only*");} if(iGroup) { await toReact("❌"); return m.reply("*🤔Weres you heading cz this is group cmd*");} return await vorterx.groupLeave(m.from,{ quoted: m });
   }
 };

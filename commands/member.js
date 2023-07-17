ffeb66ae45389e047a9a6aeb71a56ef7f64815c9
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-17 17:32:01 +0200

    v3.0.0

--- commands/member.js
+++ commands/member.js
@@ -6,10 +6,11 @@ module.exports = {
 
   if(isBotAdmin) { await toReact("❌"); return m.reply("*👮Sorry but you cannot use this cmd as you are not an admn*");
                  }   			
-  if (isGroup) { await toReact("❌"); return m.reply("*💔This command is for Admins only*");}
+  if(isGroup) { await toReact("❌"); return m.reply("*💔This command is for Admins only*");}
  	
  let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
  if (users.length == 0) return m.reply(`*🔉 Give a number you want to add to the group*`)
+     await toReact("✔️");
   await vorterx.groupParticipantsUpdate(m.from, [users], 'add').then((res) => m.reply(`🔉A member has been added successfully*`)).catch((err) => replay(`*❌Error occurred while adding a member*`))
  }
     };

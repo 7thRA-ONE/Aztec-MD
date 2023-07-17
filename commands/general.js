9a1d80c18eb90b26ece319852b0bfb876b3f6cfd
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-17 15:30:57 +0200

    vorterx

--- commands/general.js
+++ commands/general.js
@@ -7,7 +7,7 @@ module.exports = {
    description: 'An cmd to test if its on',
    start: async(vorterx, m, { prefix, pushName, toReact, botName }) => {
       await toReact("💗");
-   let aztec = fs.readFileSync("./lib/connect/vorterx.png");
+   let aztec = fs.readFileSync("./lib/connect/aztec.png");
       let msg = `*Hey ${pushName} This is ${botName} developed by Diegoson*\n\n*👾Descripto*: Whats script bot\n\n*🚦Botname*: AZTEC-MD\n\n*👾Prefix*: ${prefix}\n\n*📲Version*: 3.0.0\n\n\n*©️AZTEC-MD BY VORTERX*`;
      await vorterx.sendMessage(m.from, { image: aztec, caption: msg }, { quoted: m});
 

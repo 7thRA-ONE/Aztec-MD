b49cf343aa0da4f7e1c4bdf843f6f8509b79e0b2
HyNO <111696936+HyNO-IR@users.noreply.github.com>
2023-07-17 14:02:04 +0330

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -1,14 +1,14 @@
-// module.exports = {
-// name:"unmute",
-// description: "To Open the group",
-// category: "Group",
-// start: async(vorterx, m, { toReact, isAdmin, isBotAdmin, isGroup, pushName }) =>{
+module.exports = {
+name:"unmute",
+description: "To Open the group",
+category: "Group",
+start: async(vorterx, m, { toReact, isAdmin, isBotAdmin, isGroup, pushName }) =>{
 
-// if(isGroup) { 
-// await toReact("❌"); return m.reply("*👋 Sorry this command is for groups only*");
-// }
-// await toReact("🔉");
-//   m.reply(`*🔉Group has been opened by ${pushName}*`);
-// return await vorterx.groupSettingUpdate(m.from, "not_announcement");
-//   }
-// };
+if(isGroup) { 
+await toReact("❌"); return m.reply("*👋 Sorry this command is for groups only*");
+}
+await toReact("🔉");
+  m.reply(`*🔉Group has been opened by ${pushName}*`);
+return await vorterx.groupSettingUpdate(m.from, "not_announcement");
+  }
+};

9692cd5ffe2b09b310dadb23b91aafca08349bc2
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-17 10:35:39 +0200

    Update core.js

--- core.js
+++ core.js
@@ -44,7 +44,7 @@ const store = makeInMemoryStore({
   logger: pino().child({ level: "silent", stream: "store" }),
 });
 
-await readCommands();
+readCommands();
 const PORT = port;
 const app = express();
 let QR_GENERATE = "invalid";

2a260ae1409f3d1e34fc1e623fada08fc6cfb8f7
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-17 11:51:43 +0200

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -12,3 +12,18 @@ return await vorterx.groupSettingUpdate(m.from, "announcement");
   m.reply(`*🔉Group has been closed by ${pushName}*`);
   }
 };
+
+module.exports = {
+name:"unmute",
+description: "To Open the group",
+category: "Group",
+start: async(vorterx, m, { toReact, isAdmin, isBotAdmin, isGroup }) =>{
+
+if(isGroup) { 
+await toReact("❌"); return m.reply("*👋 Sorry this command is for groups only*");
+}
+await toReact("🔉");
+  return m.reply(`*🔉Group has been opened by ${pushName}*`);
+return await vorterx.groupSettingUpdate(m.from, "not_announcement");
+  }
+};

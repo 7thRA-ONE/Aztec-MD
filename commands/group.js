01f9e5d60243699da64c3e0aaa2431f6ab6fd49f
HyNO <111696936+HyNO-IR@users.noreply.github.com>
2023-07-17 13:43:34 +0330

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -1,40 +1,86 @@
+async function kick(vorterx, m, { text, prefix, isBotAdmin, isAdmin, mentionByTag }) {
+  if (!isAdmin) {
+    await toReact("⭕");
+    return m.reply(`*🔌This command is for admin only*`);
+  }
+  if (!isBotAdmin) {
+    await toReact("😭");
+    return m.reply(`*🔌I need to be an admin in order to use this command*`);
+  }
+  const mention = await mentionByTag();
+  if (!mention[0]) {
+    await toReact("❌");
+    return m.reply(`*🤔No user found*`);
+  }
+  await toReact("🎊");
+  await vorterx.groupParticipantsUpdate(m.from, [mention[0]], "remove");
+  await vorterx.sendMessage(m.from, { text: `*🎊User has been removed by ${pushName}*` }, { quoted: m });
+}
+
+async function group(vorterx, m, { text, prefix, isBotAdmin, isAdmin, args }) {
+  if (!isAdmin) {
+    await toReact("🔇");
+    return m.reply("*🔇This command can only be used by Admin*");
+  }
+  if (!isBotAdmin) {
+    await toReact("🔇");
+    return m.reply("*😥I need to be admin inorder to use this command*");
+  }
+
+  if (args[0] === "open") {
+    await toReact("🕳️");
+    await vorterx.groupSettingUpdate(m.from, "not_announcement")
+      .then((res) => m.reply(`*🔇Group Has Been Opened By ${pushName}*`))
+      .catch((err) => m.reply(jsonformat(err)), { quoted: m });
+  } else if (args[0] === "close") {
+    await toReact("💣");
+    await vorterx.groupSettingUpdate(m.from, "announcement")
+      .then((res) => m.reply(`*🔇Group Has been Closed By ${pushName}*`))
+      .catch((err) => m.reply(jsonformat(err)), { quoted: m });
+  }
+}
+
+async function tagall(vorterx, m, { text, prefix, isBotAdmin, isAdmin }) {
+  if (!isAdmin) {
+    await toReact("⭕");
+    return m.reply(`*🔌This is admin only command*`);
+  }
+  if (!isBotAdmin) {
+    await toReact("😭");
+    return m.reply(`*🔌I need to be an admin in order to use this command*`);
+  }
+  const participants = await vorterx.groupMetadata(m.chat).participants;
+  if (!participants) {
+    await toReact("❌");
+    return m.reply(`*🤔No user found*`);
+  }
+  if (!isMedia) {
+    var message2 = m.quoted ? m.quoted.msg : text || "No message";
+  } else {
+    message2 = "Check this Out!";
+  }
+
+  let mess = `『 *Attention Everybody* 』
+
+*⚜️ Tagged by:* @${m.sender.split("@")[0]}
+
+*🧩 Message:* ${message2}
+│\n`;
+  for (let mem of participants) {
+    mess += `┟ @${mem.id.split("@")[0]}\n`;
+  }
+  mess += `╰────────────⊰\n\n                    *Thank You*\n`;
+
+  await toReact("〽️");
+  vorterx.sendMessage(
+    m.from,
+    { text: mess, mentions: participants.map((a) => a.id) },
+    { quoted: m }
+  );
+}
+
 module.exports = {
-    name: "tagall",
-    alias: ["taga"],
-    description: "Tag all members",
-    category: "Group",
-    start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, isMedia, participants }) => {
-        // if (!isAdmin) {
-        //     await toReact("⭕");
-        //     return m.reply(`*🔌 This is admin only command*`);
-        // }
-
-        if (!isBotAdmin) {
-            await toReact("😭");
-            return m.reply(`*🔌 I need to be an admin in order to use this command*`);
-        }
-
-        let message2 = isMedia ? "Check this out!" : m.quoted ? m.quoted.msg : text || "No message";
-
-        let mess = `『 *Attention Everybody* 』\n\n*⚜️ Tagged by:* @${m.sender.split("@")[0]}\n\n*🧩 Message:* ${message2}\n`;
-
-        for (let mem of participants) {
-            mess += `┟ @${mem.id.split("@")[0]}\n`;
-        }
-
-        mess += `╰────────────⊰\n\n                    *Thank You*\n`;
-
-        try {
-            await toReact("〽️");
-            await vorterx.sendMessage(m.from, {
-                text: mess,
-                mentions: participants.map(a => a.id)
-            }, {
-                quoted: m
-            });
-        } catch (err) {
-            console.error(err);
-            await m.reply(`Failed to tag all members: ${err}`);
-        }
-    },
+  kick,
+  group,
+  tagall,
 };

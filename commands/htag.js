9f61a3354393c4d968bc3f8143752328e94c0c33
HyNO <111696936+HyNO-IR@users.noreply.github.com>
2023-07-17 14:05:01 +0330

    Create htag.js

---
+++ commands/htag.js
@@ -0,0 +1,30 @@
+module.exports = {
+  name: "hidetag",
+  alias: ["h"],
+  description: "Hide tag all members",
+  category: "Group",
+  start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, isMedia, participants }) => {
+    if (!isAdmin) {
+      await toReact("❌");
+      return m.reply(`*You* must be *Admin* in order to use this Command!`);
+    }
+    let message2;
+    if (!isMedia) {
+      message2 = m.quoted
+        ? m.quoted.msg
+        : text
+        ? text.trim()
+        : "『 *Attention Everybody* 』";
+    } else {
+      message2 =
+        "『 *Attention Everybody* 』\n\n*🎀 Message:* Check this out!";
+    }
+
+    await toReact("🎌");
+    vorterx.sendMessage(
+      m.from,
+      { text: message2, mentions: participants.map((a) => a.id) },
+      { quoted: m }
+    );
+  },
+};

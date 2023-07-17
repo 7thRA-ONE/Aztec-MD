3cdd8164d872a576d5f4bb01eabd3247b7df26d7
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-17 23:31:05 +0200

    vorterx

--- commands/profile.js
+++ commands/profile.js
@@ -82,15 +82,15 @@ let disec = m.sender.substring(3, 7)
 			let tex = "";
             if (pushName) {
              tex += `乂 *U S E R - P R O F I L E*\n\n`;
-              tex +=      `◦*Name: ${pushName}*\n\n`
-	      tex +=      `◦*Contact: ${m.sender}*\n\n`
-	      tex +=      `◦*Decription: ${yuo}*\n\n`
+              tex +=      `*◦Name*: ${pushName}\n\n`
+	      tex +=      `*◦Contact*: ${m.sender}\n\n`
+	      tex +=      `*◦Decription*: ${yuo}\n\n`
 	    }
 
       tex += `乂 *U S E R - S T A T U S*\n\n`;
-      tex +=      `◦*Balance: ${userw.xp}*\n\n`
-      tex +=      `◦*Level: ${userw.level}*\n\n`
-      tex +=      `◦Role: ${role}*\n`
+      tex +=      `*◦Balance*: ${userw.xp}\n\n`
+      tex +=      `*◦Level*: ${userw.level}\n\n`
+      tex +=      `*◦Role*: ${role}\n`
 let yui = await db.get(`${m.sender}.icon`)
 			if (yui) {
 				ppuser = yui
@@ -195,9 +195,9 @@ let disec = users.substring(3, 7)
 	      tex +=      `◦*Decription: ${yuo}*\n\n`
 			}
       tex += `乂 *U S E R - S T A T U S*\n\n`;
-      tex +=      `◦*Balance: ${userw.xp}*\n\n`
-      tex +=      `◦*Level: ${userw.level}*\n\n`
-      tex +=      `◦Role: ${role}*\n`
+      tex +=      `*◦Balance*: ${userw.xp}\n\n`
+      tex +=      `*◦Level*: ${userw.level}\n\n`
+      tex +=      `*◦Role*: ${role}\n`
 			let yui = await db.get(`${users}.icon`)
 			if (yui) {
 				ppuser = yui

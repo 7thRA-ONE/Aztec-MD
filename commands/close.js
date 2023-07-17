aecff34482d5388484f3449e18c5e679adddd5b1
HyNO <111696936+HyNO-IR@users.noreply.github.com>
2023-07-17 13:44:39 +0330

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -1,14 +1,14 @@
-module.exports = {
-name:"mute",
-description: "To close the group",
-category: "Group",
-start: async(vorterx, m, { toReact, isAdmin, isBotAdmin, isGroup, pushName }) =>{
+// module.exports = {
+// name:"mute",
+// description: "To close the group",
+// category: "Group",
+// start: async(vorterx, m, { toReact, isAdmin, isBotAdmin, isGroup, pushName }) =>{
 
-if(isGroup) { 
-await toReact("❌"); return m.reply("*👋 Sorry this command is for groups only*");
-}
-await toReact("🔉");
-    m.reply(`*🔉Group has been closed by ${pushName}*`);
-return await vorterx.groupSettingUpdate(m.from, "announcement");
-  }
-};
+// if(isGroup) { 
+// await toReact("❌"); return m.reply("*👋 Sorry this command is for groups only*");
+// }
+// await toReact("🔉");
+//     m.reply(`*🔉Group has been closed by ${pushName}*`);
+// return await vorterx.groupSettingUpdate(m.from, "announcement");
+//   }
+// };

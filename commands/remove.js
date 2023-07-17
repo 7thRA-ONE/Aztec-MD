7e8be37051a8ec4e71349cd9a07043e51df501cc
HyNO <111696936+HyNO-IR@users.noreply.github.com>
2023-07-17 15:44:11 +0330

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,43 +1,32 @@
 const config = require("../config");
 
 module.exports = {
-    name: "kick",
-    alias: ["remove"],
-    description: "remove Member from group",
-    category: "Group",
-    start: async(vorterx, m, { 
-        toReact,
-        isBotAdmin, 
-        isAdmin, 
-        mentionByTag,
-        pushName
-    }) => {
-        try {
-            if (!isAdmin) { 
-                await toReact("⭕️"); 
-                return m.reply("*🔌 This command is for admin only*");
-            }
-    
-            if (!isBotAdmin) { 
-                await toReact("😭"); 
-                return m.reply("*🔌I need to be an admin in order to use this command*");
-            }
-    
-            const users = await mentionByTag
-            
-            if (!users) { 
-                await toReact("❌"); 
-                return m.reply("*🤔No user ld found*");
-            }
-    
-            await toReact("🎊");
-            await vorterx.groupParticipantsUpdate(m.from, [users], "remove");
-            await vorterx.sendMessage(m.from,{
-                text: `*🎊User has been removed by ${pushName}*`}, {
-                quoted: m
-            });
-        } catch (e) {
-            console.log(e);
-        }
+  name: "kick",
+  alias: ["remove"],
+  description: "remove Member from group",
+  category: "Group",
+  start: async (vorterx, m, { text, prefix, toReact, isBotAdmin, isAdmin, mentionByTag }) => {
+    if (!isAdmin) {
+      await toReact("⭕");
+      return m.reply(`*🔌This is admin only command*`);
     }
-}
+    if (!isBotAdmin) {
+      await toReact("😭");
+      return m.reply(`*🔌I need to be an admin in order to use this command*`);
+    }
+    const mention = await mentionByTag;
+    if (!isAdmin) {
+      await toReact("⭕");
+      return m.reply(`*🔌This is admin only command*`);
+    }
+    const users = mention.length > 0 ? mention.map((m) => m.id) : [m.sender];
+    if (!users) {
+      await toReact("❌");
+      return m.reply(`*🤔No user ld found*`);
+    }
+    await toReact("🎊");
+    await vorterx.groupParticipantsUpdate(m.from, users, "remove");
+    const pushName = m.sender.pushname || "Unknown";
+    await vorterx.sendMessage(m.from, { text: `*🎊User has been removed by ${pushName}*` }, { quoted: m });
+  },
+};

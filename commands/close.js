b0df930c91b4b9b23cdba6988ea4ad6016ecd410
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-17 11:12:03 +0200

    Create close.js

---
+++ commands/close.js
@@ -0,0 +1,19 @@
+module.exports = {
+name:"mute",
+description: "To close the group",
+category: "Group",
+start: async(vorterx, m, { isAdmin, toReact, isBotAdmin, isGroup }) =>{
+
+if(isAdmin) {
+await toReact("❌"); return m.reply("*👮This command is only for my owner*");
+}
+if(isBotAdmin) {
+ await toReact("❌"); return m.reply("*😭Im not an admin so l can't procedure*");
+}
+if(isGroup) { 
+await toReact("❌") return m.reply("*👋 Sorry this command is for groups only*");
+}
+await toReact("🔉")
+return await vorterx.groupSettingUpdate(m.from, "announcement");
+  }
+);

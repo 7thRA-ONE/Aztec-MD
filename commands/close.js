f57586d52817899535208c6d93d79da366753c7c
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-07-17 11:31:14 +0200

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -4,8 +4,7 @@ description: "To close the group",
 category: "Group",
 start: async(vorterx, m, { toReact, isBotAdmin, isGroup }) =>{
 
-if(isBotAdmin) {
- await toReact("❌"); return m.reply("*😭Im not an admin so l can't procedure*");
+if(isAdmin(m.from, m.user)) { await toReact("❌"); return m.reply("*😭Im not an admin so l can't procedure*");
 }
 if(isGroup) { 
 await toReact("❌"); return m.reply("*👋 Sorry this command is for groups only*");

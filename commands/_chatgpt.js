6fb5a48fe470375fdc23ecaa9410aa30232df023
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-19 00:19:03 +0200

    vorterx

---
+++ commands/_chatgpt.js
@@ -0,0 +1,26 @@
+const ChatGpT = require("../config");
+
+module.exports = {
+  name: "gpt",
+  alias:["ai"],
+  description: "To search anonymous act",
+  category: "Search",
+  start: async(vorterx, m, {prefix, toReact, pushName }) => {
+
+  if(!process.env.ChatGpT) { await toReact("⛔"); return m.reply("⛔Error no gpt api has been excuted yet");
+                           }
+    await toReact("🤖");
+    if(!process.env.ChatGpT) return
+    const api = new ChatGpT({
+      apiKey: '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'
+    })
+  
+    const res = await api.sendMessage(match)
+
+  
+  
+ return await vorterx.sendMessage(m.from, {text: res.text});
+
+
+
+});

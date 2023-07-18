653b84db9cfc2ee1c66af1871703bcf60bf2cdc2
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-18 23:14:07 +0200

    vorterx

---
+++ commands/instagram.js
@@ -0,0 +1,16 @@
+const { Insta } = require("../lib");
+
+module.exports = {
+  name: "insta",
+  alias: ["ig"],
+  description: "To download Instagramer videos",
+  category: "Download",
+  start: async(vorterx, m, {prefix, pushName, toReact, isCreator, text }) => {   
+        const { Insta } = require('../lib')
+if(text) { await toReact("⛔"); return m.reply("*🔌Need an Instagram Url*");
+         }
+let response = await Insta(text)
+for (let i=0;i<response.length;i++) {
+await vorterx.sendFileUrl(m.from, response[i], `*Downloaded Media from instagram.*`, vortii)
+}
+    });

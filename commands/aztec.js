c7aeaed860ed6f20cdf901a0bbd060cde66e91c0
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-18 20:34:45 +0200

    vorterx

--- commands/aztec.js
+++ commands/aztec.js
@@ -8,9 +8,10 @@ module.exports = {
   start: async(vorterx, m, { prefix, toReact, pushNme }) => {
     
     await toReact("🛸");
+    src = `https://i.ibb.co/6WV7wbc/20230718-084726.png`;
     let axios = await axios.get(`https://api.github/repos/Vorterx/Aztec-MD`);
     let git = `Hey ${pushName}` 
      git+= ` *乂 A Z T E C  M D- R E P O 乂*\n\n*🌟Total Stars*: ${data.stargazers_count}\n\n*🛸Users* ${data.forks_count}\n\n*🌲Repo*: ${data.html_url}\n\n*🌲Whatsapp group*: none\n\n*🌲Owner*: _27686881509_`,
-       return await vorterx.sendMessage(m.from, {image: url, Caption: git}, {quoted: m});
+       return await vorterx.sendMessage(m.from, {image: {url: src}, caption: git}, {quoted: m});
   }
 };

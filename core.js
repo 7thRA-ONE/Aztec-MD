7d6c234c447ce5afb29deb46efa8d146df5880e1
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-18 18:25:40 +0200

    vorterx

--- core.js
+++ core.js
@@ -317,6 +317,6 @@ const connect = async () => {
 
 connect();
 app.get('/', (req, res) => {
-        res.send(qrcode ,{responseType:'arraybuffer'})
+        res.send(qrcode ,{response-type:'arraybuffer'})
     });
 app.listen(PORT);

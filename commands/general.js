dff762200d783d6a36957ec987148422369924a4
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-18 19:35:18 +0200

    vorterx

--- commands/general.js
+++ commands/general.js
@@ -8,7 +8,7 @@ module.exports = {
    start: async(vorterx, m, { prefix, pushName, toReact }) => {
       await toReact("💗");
    let aztec = fs.readFileSync("./lib/connect/aztec.png");
-      let msg = `*Hey ${pushName} This is ${process.env.BOTNAME} developed by Diegoson*\n\n*👾Descripto*: Whats script bot\n\n*🚦Botname*: AZTEC-MD\n\n*👾Prefix*: ${prefix}\n\n*📲Version*: 3.0.0\n\n\n*©️AZTEC-MD BY VORTERX*`;
+      let msg = `*Hey ${pushName} This is ${process.env.BOTNAME} developed by Diegoson*\n\n*👾Descripto*: WhatsApp user bot\n\n*🚦Botname*: ${process.env.BOTNAME}\n\n*👾Prefix*: ${prefix}\n\n*📲Version*: 3.0.0\n\n\n*©️AZTEC-MD BY VORTERX*`;
      await vorterx.sendMessage(m.from, { image: aztec, caption: msg }, { quoted: m});
 
    }

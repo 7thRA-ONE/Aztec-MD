c542df3886d47a6c2eccafc9b3e428e41b153383
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-18 20:45:57 +0200

    v3.0.0

--- commands/aztec.js
+++ commands/aztec.js
@@ -1,4 +1,5 @@
-const axios = require("axios");
+require("../config")
+const axios = require("axios")
 
 module.exports = {
   name: "sc",
@@ -9,9 +10,9 @@ module.exports = {
     
     await toReact("🛸");
     src = `https://i.ibb.co/6WV7wbc/20230718-084726.png`;
-    let axios = await axios.get(`https://api.github/repos/Vorterx/Aztec-MD`);
+    let vorterxi = await axios.get(`https://api.github/repos/Vorterx/Aztec-MD`);
     let git = `Hey ${pushName}` 
-     git+= ` *乂 A Z T E C  M D- R E P O 乂*\n\n*🌟Total Stars*: ${data.stargazers_count}\n\n*🛸Users* ${data.forks_count}\n\n*🌲Repo*: ${data.html_url}\n\n*🌲Whatsapp group*: none\n\n*🌲Owner*: _27686881509_`,
+     git+= ` *乂 A Z T E C  M D- R E P O 乂*\n\n*🌟Total Stars*: ${vorterxi.stargazers_count}\n\n*🛸Users* ${vorterxi.forks_count}\n\n*🌲Repo*: ${vorterxi.html_url}\n\n*🌲Whatsapp group*: none\n\n*🌲Owner*: _27686881509_`,
         await vorterx.sendMessage(m.from, {image: {url: src}, caption: git}, {quoted: m});
   }
 };

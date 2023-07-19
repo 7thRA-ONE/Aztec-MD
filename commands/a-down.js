47cba04da679b57600e77816e41ab723bd459c82
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-19 18:12:11 +0200

    vorterx

--- commands/a-down.js
+++ commands/a-down.js
@@ -21,9 +21,10 @@ const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`
 	
 	const apkSize = parseInt(data.size);
 	if(apkSize > 100) return m.reply(`⛔This App is too much large.`);
+	  await toReact("♻️");
        const url = data.dllink;
        
-	 let  inf  ="*乂 A P P R A N D M - D O W N L O A D E R*";\n\n
+	 let  inf  ="*乂 A P P R A N D M - D O W N L O A D E R*"\n\n;
          inf +="\n*📚Name*: " +data.name;
          inf +="\n*📦Dev*: " +data.package;
          inf +="\n*⬆️Last Update*: " +data.lastup;

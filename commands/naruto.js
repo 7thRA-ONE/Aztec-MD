25ff4a0beaaa3e802bcc9da4738e53390d11d37d
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-20 00:14:38 +0200

    vorterx

---
+++ commands/naruto.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+module.exports = {
+  name: "naruto",
+  description: "To see uzumakis status",
+  category: "Anime",
+  start: async(vorterx, m, { prefix, toReact, getJson}) => {
+
+      await toReact("👲");
+  let vorterxi = await axios.get("https://raw.githubusercontent.com/mask-sir/api.mask-ser/main/Naruto.json")
+        let url =  vorterxi.data.result[Math.floor(Math.random() * vorterxi.data.result.length)];
+        return await vorterx.sendMessage(m.from,{video :{url : url } , caption: "«Gᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ᴠᴏʀᴛᴇx" }, { quoted: citel })
+}
+};

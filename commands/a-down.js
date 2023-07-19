4263e5cb11db039a4a20cf8902575b393a660d01
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-19 18:36:15 +0200

    vorterx

--- commands/a-down.js
+++ commands/a-down.js
@@ -57,5 +57,7 @@ const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`
       if (err) { console.error('Erasing this app error:', err); } else { console.log('App has been deleted successfully'); } });
   }) .catch(error => {
 	fs.unlink(filePath)
-    return m.reply('*Could not find the app*')
-  });
+    return m.reply('*Could not find the app*'));
+  }
+};
+		

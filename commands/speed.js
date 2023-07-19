b02af2eeb0909433e89dd96a77318e20c7838fb7
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-20 00:23:15 +0200

    vorterx

---
+++ commands/speed.js
@@ -0,0 +1,17 @@
+const config = require('../config')
+const fs = require("fs");
+
+module.exports = {
+   name: 'ping',
+   category: 'user',
+   description: 'to check ping',
+start: async(vorterx, m, { prefix, toReact }) => {
+  await toReact("💘");
+        var start = new Date().getTime();
+	var msg = await m.reply('*𝆺𝅥 ʀᴜɴɪɴɢ 𝆺𝅥*');
+	var end = new Date().getTime();
+	var responseTime = end - start;
+	await vorterx.edit(`*☇ ꜱᴩᷨᴇͦᴇͭᴅ ☁ : ${responseTime}ms*`);
+}
+};
+  

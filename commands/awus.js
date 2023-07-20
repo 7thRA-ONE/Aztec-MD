a0180ab878bd803708db6f46371951f24693be4c
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-20 17:32:48 +0200

    vorterx

--- commands/awus.js
+++ commands/awus.js
@@ -7,11 +7,12 @@ category: "Games",
 start: async(vorterx, m, { prefix, toReact }) => {
 
 await toReact("😘");
+  aztec = "https://i.ibb.co/ZmxJ3Sr/20230720-172644.png",
 let vorterxi = await axios.get(`https://favqs.com/api/qotd`);
 const reply = `
 📝 *Content:* ${quoo.data.quote.body}
 *✍️ Author:* ${quoo.data.quote.author}
               `
-vorterx.sendMessage(m.from, {image: amarok },  {quoted:m});
+vorterx.sendMessage(m.from, {image: { url: aztec }}, {quoted:m});
   }
 };

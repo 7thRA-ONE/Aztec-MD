34031445aaabe41a1d258604809fcdeb0ccb5e15
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-21 00:05:57 +0200

    v3.0.0

--- commands/close.js
+++ commands/close.js
@@ -1,11 +1,12 @@
 module.exports = {
-name:"mute",
+name:"close",
+alias: ["mute"],
 description: "To close the group",
 category: "Group",
 start: async(vorterx, m, { toReact, isAdmin, isBotAdmin, isGroup, pushName }) =>{
 
-if(isGroup) { 
-await toReact("❌"); return m.reply("*👋 Sorry this command is for groups only*");
+if(m.isGroup) { 
+await toReact("❌"); return m.reply("*👋 Sorry this command is for admins only*");
 }
 await toReact("🔉");
     m.reply(`*🔉Group has been closed by ${pushName}*`);

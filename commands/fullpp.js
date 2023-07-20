6704f8d179abaf0a72ff7959cd63db2822b58239
HyNO <111696936+HyNO-IR@users.noreply.github.com>
2023-07-20 13:19:36 +0330

    Update fullpp.js

--- commands/fullpp.js
+++ commands/fullpp.js
@@ -3,10 +3,12 @@ module.exports = {
     description: "set full profile picture",
     category: "user",
 start: async(vorterx, m, { quoted,toReact }) => {
-			if (!/image/.test(mime))
+			if (!/image/.test(mime)) {
 				return await toReact("🔖"); m.reply('_Reply to a image._');
+			}else{
 	const media = await vorterx.reply_message
 	await vorterx.updateProfilePicture(m.user_id, media)
 	await toReact("👜"); m.reply('_Successfully Profile Picture Updated_');
 	}
 }
+};

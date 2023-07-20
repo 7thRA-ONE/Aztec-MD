9d8963bb4796c045f61c950df47f791f651e101f
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-21 00:03:10 +0200

    vorterx

--- commands/open.js
+++ commands/open.js
@@ -4,8 +4,8 @@ description: "To Open the group",
 category: "Group",
 start: async(vorterx, m, { toReact, isAdmin, isBotAdmin, isGroup, pushName }) =>{
 
-if(isGroup) { 
-await toReact("❌"); return m.reply("*👋 Sorry this command is for groups only*");
+if(m.isGroup) { 
+await toReact("❌"); return m.reply("*👋 Sorry this command is for admins only*");
 }
 await toReact("🔉");
   m.reply(`*🔉Group has been opened by ${pushName}*`);

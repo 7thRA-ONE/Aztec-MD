af9c00ddaaaca16c5ff49dbb7f817726393e08ca
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-21 16:52:51 +0200

    created

--- commands/video.js
+++ commands/video.js
@@ -29,7 +29,7 @@ module.exports = {
           m.from,
           {
             video: { url: ytaud.videoUrl },
-            caption:`  *乂 V I D E O - D O W  N L D*\n\n
+            caption:`  *乂 V I D E O S  - D O W  N L O A D*\n\n
  *🌲Name*: ${song.title}\n\n
  *🌲Size*: ${song.size}\n\n
  *🌲Botname*: ${process.env.BOTNAME}`,

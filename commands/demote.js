1b18ad7528a6fd46e28e49626fa4969cce9ff857
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-21 15:55:23 +0200

    vorterx

---
+++ commands/demote.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "demote",
+  description: "To demote a member",
+  category: "Group",
+  start: async(vorterx, m, { prefix, isGroup, pushName, isAdmin, isBotAdmin, toReact, mentionByTag, text }) => {
+    
+if (!m.isGroup) { await toReact("❌"); return m.reply("*Sorry its a group command.Couldn't process the request*");
+	      }
+if (!isAdmin) { await toReact("❌"); return m.reply("*This is an Admin command only*");
+              }
+if (!isBotAdmin) { await toReact("❌"); return m.reply("*Cannot execute without being admin*");
+                 }
+	try {
+                            let mention = mentionByTag
+      let users = await (mention[0]) || m.msg.contextInfo.participant
+	  if (!users) return m.reply("*Couldn't find any userID in context*")
+    await toReact("👮");
+        await vorterx.groupParticipantsUpdate(m.from, [users], 'demote')
+         vorterx.sendMessage(m.from,{text:`@${users.split("@")[0]} *has been demoted by ${pushName}*`,contextInfo: { mentionedJid: [users] }})
+        } catch { 
+		return m.reply(`*Sorry failed to demote user could not be found*`);
+    }
+         },
+};

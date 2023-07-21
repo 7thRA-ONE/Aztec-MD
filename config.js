611c5db32880855e820a97f8ef155a49ddc485c2
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-21 21:24:04 +0200

    vorterx

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ if (!vorterx) {
 global.owner = vorterx.split(",");
 global.mongodb = process.env.MONGODB || "none";
 global.botName = process.env.BOTNAME || "Aᴢᴛᴇᴄ-Mᴅ"; // change botName
-global.prefix = process.env.PREFIX || ".";
+global.prefix = process.env.PREFIX || "-";
 global.chatGpT = process.env.ChatGpT || ""; //chatgpt api key here so it will respond
 global.sessionId = process.env.SESSION_ID || "vorterx"; //change session 
-global.image_url = process.env.IMAGE_URL || "https://i.ibb.co/v47d4BL/IMG-20230429-WA0021.jpg"; //change image 
+global.IMAGE_URL = process.env.IMAGE_URL || 'https://i.ibb.co/v47d4BL/IMG-20230429-WA0021.jpg'; //change image 
 global.port = process.env.PORT || 8081;
 
 let file = require.resolve(__filename);

ad72efcc7481bd157e2add15b02a61536b64720a
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-21 20:38:31 +0200

    vorterx

---
+++ commands/owner.js
@@ -0,0 +1,37 @@
+module.exports = {
+  name: "owner",
+  description: "Owner",
+  category: "User",
+  start: async(vorterx, m, { prefix, toReact, pushName }) => {
+
+    await toReact("✔️");
+  
+const config = require('../config');
+        const thmb = await getBuffer(global.THUMB_IMAGE)
+        const vcard = 'BEGIN:VCARD\n' +
+            'VERSION:3.0\n' +
+            'FN:' + config.vorterx + '\n' +
+            'ORG:;\n' +
+            'TEL;type=CELL;type=VOICE;waid=' + config.vorterx + ':+' + config.vorterx + '\n' +
+            'END:VCARD'
+        let buttonMessaged = {
+            contacts: { displayName: config.vorterx, contacts: [{ vcard }] },
+            contextInfo: {
+                externalAdReply: {
+                    title: config.vorterx,
+                    body: 'Aztec MD.',
+                    renderLargerThumbnail: true,
+                    thumbnailUrl: ``,
+                    thumbnail: thmb,
+                    mediaType: 1,
+                    mediaUrl: '',
+                    sourceUrl: `https://wa.me/+` + owner + '?text=Hii+bro,I+am+' + pushName,
+                },
+            },
+        };
+        return await vorterx.sendMessage(m.from buttonMessaged, {
+            quoted: m,
+        });
+
+              }
+};

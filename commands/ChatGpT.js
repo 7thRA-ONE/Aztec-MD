49c7dbd36447e872367c5f76ccb545b0bf0d1973
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 23:51:36 +0200

    vorterx

---
+++ commands/ChatGpT.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "gpt",
+  alias: ["ai"],
+  description: "Gpt",
+  category: "User",
+  start: async(vorterx,m, {prefix,toReact,text}) => {
+    
+var fetch = require('node-fetch');
+    await toReact("🤖");
+  var apii = await fetch(`https://api.botcahx.live/api/search/openai-chat?text=${text}&apikey=${btc}`)
+  var res = await apii.json()
+  await m.reply(res.m)
+} catch (err) {
+  console.error(err)
+    return m.reply(`*Error occurred while processing*`);
+   }
+};
+

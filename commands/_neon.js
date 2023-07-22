00149c972c074ac7662d0a2d371ea7ee82de48a4
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 21:56:37 +0200

    vorterx

---
+++ commands/_neon.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "neon",
+  description: "Textpro",
+  category: "Textpro",
+  start: async(vorterx,m,{ prefix, toReact,text }) => {
+
+    if(!text) { await toReact("⛔"); return m.reply("*Provide me a query ex neon aztec*");
+              }
+    const maker = require('mumaker');
+    const config = require("../config");
+    await toReact("💘");
+maker.textpro("https://textpro.me/neon-text-effect-online-963.html",[
+      `${text}`,]).then((data) =>
+                  vorterx.sendMessage(m.from, { image: {url: data}, caption: `Cʀᴇᴀᴛᴇᴅ Bʏ: ${process.env.BOTNAME}`}, { quoted: m}));
+  }
+};

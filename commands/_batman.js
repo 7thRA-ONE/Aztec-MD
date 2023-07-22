f287bc98a440f3f5dbb3c1d435df32a7a62f42d2
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 19:00:52 +0200

    amarok

--- commands/_batman.js
+++ commands/_batman.js
@@ -6,7 +6,6 @@ module.exports = {
 
     if(!text) { await toReact("⛔"); return m.reply("*Provide me a query ex batman aztec*");
               }
-    const axios = require("axios");
     const config = require("../config");
     await toReact("💘")
     maker.textpro("https://textpro.me/make-a-batman-logo-online-free-1066.html",[

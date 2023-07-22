87952a45bde9bcdeaaeb48986434ecd7df83e5c8
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 10:38:04 +0200

    vorterx

--- commands/Slayer.js
+++ commands/Slayer.js
@@ -8,9 +8,9 @@ module.exports = {
   start: async(vorterx, m, { prefix, toReact}) => {
 
       await toReact("🫀");
-  let loki = await axios.get("https://gist.github.com/lokixjs/3e582b8cbb5068f5294fe14d26528940/raw");
+  let loki = await axios.get("https://gist.github.com/lokixjs/3e582b8cbb5068f5294fe14d26528940/raw")
         let url =  loki.data.result[Math.floor(Math.random() * loki.data.result.length)];
-        return await vorterx.sendMessage(m.from,{video :{url : url } , caption: "𝐃𝐞𝐦𝐨𝐧 𝐒𝐥𝐚𝐲𝐞𝐫" }, { quoted: m })
+         vorterx.sendMessage(m.from,{video :{url : url } , caption: "𝐃𝐞𝐦𝐨𝐧 𝐒𝐥𝐚𝐲𝐞𝐫" }, { quoted: m })
    }
 };
 

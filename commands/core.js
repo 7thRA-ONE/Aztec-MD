845aaf7fb3bab57a75d5602c4a36e9d8c099b26f
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 14:01:06 +0200

    vorterx

--- commands/core.js
+++ commands/core.js
@@ -42,7 +42,7 @@ module.exports = {
 
 const keys = Object.keys(category);
       for (const key of keys) {
-        amarok += `*┌─『${key.toLowerCase()}*』─❖\n\n${tiny(category)[key]
+        amarok += `*┌─『${tiny(key.toLowerCase())}*』─❖\n\n${category[key]
           .map((cmd) => ` |${prefix + cmd.name}`)
           .join("\n")}\n\n└─────────◉\n\n`;
       }

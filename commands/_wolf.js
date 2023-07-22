8d4279cbd9bdc252368797c350eb0f6dc1c9d3d4
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 22:12:32 +0200

    multi auth

---
+++ commands/_wolf.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "wolf",
+  description: "Textpro",
+  category: "Textpro",
+  start: async(vorterx,m,{ prefix, toReact,text }) => {
+
+    if(!text) { await toReact("⛔"); return m.reply("*Provide me a query ex wolf aztec*");
+              }
+    const maker = require('mumaker');
+    const config = require("../config");
+    await toReact("💘");
+    maker.textpro("https://textpro.me/create-wolf-logo-galaxy-online-936.html",[
+      `${text}`,]).then((data) =>
+                  vorterx.sendMessage(m.from, { image: {url: data}, caption: `Cʀᴇᴀᴛᴇᴅ Bʏ: ${process.env.BOTNAME}`}, { quoted: m}));
+  }
+};

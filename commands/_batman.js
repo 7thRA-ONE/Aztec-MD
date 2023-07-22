233b9d4aa2307f0267bcfad87b105fc19aea9585
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 19:19:14 +0200

    vorterx

--- commands/_batman.js
+++ commands/_batman.js
@@ -6,6 +6,7 @@ module.exports = {
 
     if(!text) { await toReact("⛔"); return m.reply("*Provide me a query ex batman aztec*");
               }
+    const maker = require('mumaker');
     const Config = require("../config");
     await toReact("💘");
     maker.textpro("https://textpro.me/make-a-batman-logo-online-free-1066.html",[

df63dd65b3a8fc162709c6d81a6dbcbf4e418129
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 16:24:43 +0200

    vorterx

---
+++ commands/_anemi.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: "genshin",
+  description: "Anime lovers",
+  category: "Anime",
+  start: async(vorterx,m {prfix, toReact, pushName, text}) => {
+
+const genshin = require("genshin-api");
+a = text.toLowerCase();
+    await toReact("🔥");
+const anime = await genshin.Characters(text)
+let txt = ""
+txt += `🎀 *Name:* ${anime.name}\n`
+txt += `🌲 *Title:* ${anime.title}\n`
+txt += `💠 *Vision:* ${anime.vision}\n`
+txt += `🏹 *Weapon:* ${anime.weapon}\n`
+txt += `💮 *Gender:* ${anime.gender}\n`
+txt += `🌏 *Nation:* ${anime.nation}\n`
+txt += `💘 *Affiliation:* ${anime.affiliation}\n`
+txt += `🌟 *Rarity:* ${anime.rarity}\n`
+txt += `❄️ *Constellation:* ${anime.constellation}\n`
+txt += `📖 *Description:* ${anime.description}\n`
+urll = `https://api.genshin.dev/characters/${a}/portrait`
+await vorterx.sendMessage(m.from,{image:{url:urll}, caption:txt},{quoted:m});
+} catch (err) {
+console.log(err)
+return m.reply('Error');
+}
+   };
+

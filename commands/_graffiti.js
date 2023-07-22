f4f43ac7ef43ed60fa3a5ffdea6adb5d484f77ea
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 21:43:52 +0200

    vorterx

--- commands/_graffiti.js
+++ commands/_graffiti.js
@@ -1,15 +1,15 @@
 module.exports = {
-  name: "avengers",
-  description: "Textpro avengers",
+  name: "graffiti",
+  description: "Textpro",
   category: "Textpro",
   start: async(vorterx,m,{ prefix, toReact,text }) => {
 
-    if(!text) { await toReact("⛔"); return m.reply("*Provide me a query ex avengers aztec*");
+    if(!text) { await toReact("⛔"); return m.reply("*Provide me a query ex graffiti aztec*");
               }
     const maker = require('mumaker');
     const config = require("../config");
     await toReact("💘");
-    maker.textpro("https://textpro.me/create-3d-avengers-logo-online-974.html",[
+    maker.textpro("https://textpro.me/create-a-cool-graffiti-text-on-the-wall-1010.html",[
       `${text}`,]).then((data) =>
                   vorterx.sendMessage(m.from, { image: {url: data}, caption: `Cʀᴇᴀᴛᴇᴅ Bʏ: ${process.env.BOTNAME}`}, { quoted: m}));
   }

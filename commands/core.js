c59fb6e08b763c7d198a5f9c14905e9aa2243e93
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 07:17:46 +0200

    vorterx

--- commands/core.js
+++ commands/core.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const { tiny } = require("../lib/fonts.js");
+//const { tiny } = require("../lib/fonts.js");
 
 module.exports = {
   name: "menu",
@@ -41,7 +41,7 @@ module.exports = {
 
 const keys = Object.keys(category);
       for (const key of keys) {
-        amarok += `*┌─『${tiny(key.toUpperCase)()}*』─❖\n\n${category[key]
+        amarok += `*┌─『${key.toUpperCase()}*』─❖\n\n${category[key]
           .map((cmd) => ` |${prefix + cmd.name}`)
           .join("\n")}\n\n└─────────◉\n\n`;
       }

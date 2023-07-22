1150c0ed270c7cd17ee1ed373b93ab8403e84cfc
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 23:34:21 +0200

    vorterx

---
+++ commands/setStatus.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "setstus",
+  description: "To set status",
+  category: "Owner",
+  start: async(vorterx,m,(prefix,toReact, text}) => {
+
+               if(!text) { await toReact("⛔"); return m.reply("*Please give me  a query*");
+                         }
+      await toReact("💘");
+    await vorterx.setStatus(m.from,text)
+       return m.reply("*Successfully Wa Status has been set*");
+  }
+               };

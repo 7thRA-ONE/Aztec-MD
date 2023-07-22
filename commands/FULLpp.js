7ba68f060342e0d2cb3b3165190940a4cf15033c
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 11:43:27 +0200

    vorterx

--- commands/FULLpp.js
+++ commands/FULLpp.js
@@ -8,7 +8,7 @@ module.exports = {
 
     if(text) { await toReact("⛔"); return m.reply("*Please reply to an image*");
              }
-    let media = await vorterx.downloadAndSaveMediaMessage(quoted);
+    let media = await vorterx.downloadAndSaveMediaMessage();
     await updateProfilePicture(m.user, media, m);
     return await m.reply("*Profile picture updated successfully*");
   }

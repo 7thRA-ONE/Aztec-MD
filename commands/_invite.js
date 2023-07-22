66caff6cbeb1371a5a2792100a26763e25e5d53e
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-22 09:45:57 +0200

    vorterx

--- commands/_invite.js
+++ commands/_invite.js
@@ -4,9 +4,7 @@ module.exports = {
   category: "Group",
   start: async(vorterx, m, { prefix, toReact, isAdmin, isBoAdmin, isGroup }) => {
 
-  if(m.isGroup) { await toReact("⭕"); return m.reply("*Sorry this is admins command only*");
-                }
-    await toReact("✔️");
+  await toReact("✔️");
     let response = await vorterx.groupInviteCode(m.from)
                 vorterx.sendText(m.from, `https://chat.whatsapp.com/${response}\n\n*乂Group* : ${groupMetadata.subject}`, m, { detectLink: true });
             }

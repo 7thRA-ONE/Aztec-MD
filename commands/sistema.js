9bfdae8dda2a19b686bfee6637edd126be89f880
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-23 16:25:27 +0200

    vorterx

---
+++ commands/sistema.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "system",
+  description: "To Check the system status",
+  category: "user",
+  start: async(vorterx,m,{prefix,text,toReact}) => {
+
+  const os = require('os');
+  const speed = require('performance-now');
+  const latensi = speed() - speed();
+    await toReact("📟");
+  let aztec =  `*乂 SYSTEM - STATUS*\n\n`;
+  aztec += `❲❒❳ *BotName :* ${process.env.BOTNAME}\n`;
+  aztec += `❲❒❳ *Version :* 3.0.0`;
+  aztec += `❲❒❳ *RAM :* _${formatp(os.totalmem() - os.freemem())}/${formatp(os.totalmem())}_\n`;
+  aztec += `❲❒❳ *Speed : _${latensi.toFixed(4)}sec_*\n`;
+  aztec += `❲❒❳ *Runtime :* _${runtime(process.uptime())}_\n`;
+  aztec += `❲❒❳ *Platform :* ${os.platform()}.com\n`;
+  aztec += `❲❒❳ *Platform ID :* ${os.hostname()}\n\n\n*©vorterx-team*`;
+m.reply(aztec)
+      }
+};
+  

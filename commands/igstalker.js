8d55f161d07a53e82415c8861b5779fab034d394
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-23 11:07:37 +0200

    vorterx

---
+++ commands/igstalker.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "igstalker",
+  description: "Instagram",
+  category: "Search",
+  start: async(vorterx,m,{prefix,toReact, text}) => {
+    
+if (!args[0]) { await toReact("⛔"); return m.reply("*Please give me use ig id*");
+              }
+               const fg = require('api-dylux');
+    await toReact("📍");
+    let res = await fg.igStalk(args[0])
+    let aztec = `
+*乂 I G S T A L K E R*
+▢ *🧕Name:* ${res.name} 
+▢ *👩‍🚀Username:* ${res.username}
+▢ *👥Followers:* ${res.followersH}
+▢ *🫂Following:* ${res.followingH}
+▢ *📍Bio:* ${res.description}
+▢ *📥Posts:* ${res.postsH}
+▢ *📩Email:* ${res.email}
+`
+     await vorterx.sendMessage(m.from, {image: { url: res.profilePic }, caption: aztec }, {quoted: m})
+      }
+};

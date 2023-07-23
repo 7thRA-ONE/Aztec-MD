57ae048af963547346d069928e67362d9db05acc
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-23 18:19:05 +0200

    vorterx

---
+++ commands/song.js
@@ -0,0 +1,33 @@
+  module.exports = {
+    name: "song",
+    description: "To download songs",
+    category: "Download",
+    start: async(vorterx,m,{prefix,toReact,text}) => {
+    
+   const fs = require("fs");
+if (!text) { await toReact("⛔"); return m.reply("*Provide me a song name ex hope by xxx*");
+}
+const xeonplaymp3 = require('../lib/ytdl2');
+await toReact("🎶");
+let yts = require("youtube-yts")
+        let search = await yts(text)
+        let anup3k = search.videos[0]
+const pl= await xeonplaymp3.mp3(anup3k.url)
+await vorterx.sendMessage(m.from,{
+    audio: fs.readFileSync(pl.path),
+    fileName: anup3k.title + '.mp3',
+    mimetype: 'audio/mp4', ptt: true,
+    contextInfo:{
+        externalAdReply:{
+            title:anup3k.title,
+            body: botname,
+            thumbnail: await fetchBuffer(pl.meta.image),
+            mediaType:2,
+            mediaUrl:anup3k.url,
+        }
+
+    },
+},{quoted:m})
+await fs.unlinkSync(pl.path)
+}
+    };

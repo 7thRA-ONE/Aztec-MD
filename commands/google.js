91700e66a6625339d1e456364bde023923eedf87
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-23 14:18:32 +0200

    vorterx

---
+++ commands/google.js
@@ -0,0 +1,19 @@
+const google = require('google-it');
+
+module.exports = {
+   name: "google",
+   description: "Search random stuff",
+   category: "Search",
+   start: async(vorterx,m,{prefix,toReact, text}) => {
+
+
+google({'query': text}).then(res => {
+let aztec = `*乂 GOOGLE SEARCH TERM* : ${text}\n\n`
+for (let g of res) {
+aztec += `⭔ *Title* : ${g.title}\n`
+aztec += `⭔ *Description* : ${g.snippet}\n`
+aztec += `⭔ *Link* : ${g.link}\n\n────────────────────────\n\n`
+} 
+m.reply(aztec))
+     } 
+};

d25c2451928d9b50e703c544ab7b761d773478ff
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-07-23 23:14:59 +0500

    Multi Menu Tried--1

--- commands/core.js
+++ commands/core.js
@@ -39,20 +39,42 @@ module.exports = {
           category[info.category].push(info);
         }
       }
-      let amarok = `┏━━⟪ ${tiny(process.env.BOTNAME)} ⟫━⦿
-┃ ✗ User: ${tiny(pushName)}
-┃ ✗ Botname: ${tiny(process.env.BOTNAME)}
-┃ ✗ Prefix: ${tiny(prefix)}
-┃ ✗ Runtime: ${tiny(runtime(process.uptime()))}
-┃ ✗ Time: ${tiny(time)}
-┃ ✗ Date: ${tiny(date)}
-┗━━━━━━━━━━⦿\n\n`;
+//-----------------------------------------------------------------------
+       var up_up, up_mid, up_btm, ctgry_L, ctgry_R, cmd_L, ctgry_end
+            let default_menu = 0 ;
+            if (Config.menu === '') { default_menu = Math.floor(Math.random() * 3) + 1;      }
+            
+            if (default_menu == 1 || Config.menu.trim().startsWith("1") || Config.menu.toLowerCase().includes("aztec")) {            
+              up_up =  `┏━━⟪ ${tiny(process.env.BOTNAME)} ⟫━━⦿`
+              up_mid = `┃ ✗`
+              up_btm = `┗━━━━━━━━━━━━━━━⦿`
+              ctgry_L  = `┌──『`
+              ctgry_R  = `』──❖\n`
+              cmd_L =    ` | `
+              ctgry_end =`\n└──────────────◉`
+              
+            }
+
+
+
+
+
+      
+      let amarok = `${up_up}
+${up_mid} User: ${tiny(pushName)}
+${up_mid} Botname: ${tiny(process.env.BOTNAME)}
+${up_mid} Prefix: ${tiny(prefix)}
+${up_mid} Runtime: ${tiny(runtime(process.uptime()))}
+${up_mid} Time: ${tiny(time)}
+${up_mid} Date: ${tiny(date)}
+${up_btm}\n
+`;
 
 const keys = Object.keys(category);
       for (const key of keys) {
-        amarok += `*┌─『${tiny(key.toLowerCase())}*』─❖\n\n${category[key]
-          .map((cmd) => ` |${prefix + cmd.name}`)
-          .join("\n")}\n\n└─────────◉\n\n`;
+        amarok += `${ctgry_L} ${tiny(key.toLowerCase())} ${ctgry_R}  \n\n${category[key]
+          .map((cmd) => ` ${cmd_L}${prefix + cmd.name}`)
+          .join("\n")}\n${ctgry_end}\n`;
       }
       amarok += `*©vorterx-team*`;
 await vorterx.sendMessage(m.from, { image: aztec, caption: tiny(amarok )}, { quoted: m});

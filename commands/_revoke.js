79ef59ca8fd6b850680fd24decaf8321e46fe121
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-25 00:30:00 +0200

    vorterx

---
+++ commands/_revoke.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "revoke",
+  description: "To reset group link",
+  category: "Group",
+  start: async(vorterx,m,{prefix,toReact,isGroup}) => {
+
+  if(!isGroup) { await toReact("⛔"); return vorterx.sendMessage(m.from,{text: '*This command is for Admins only*'}, {quoted:m});
+               }
+    await toReact("✅");
+    await vorterx.groupRevokeInvite(m.from).then((res) => {
+            m.reply(`*🔉 Group link has been reserted successfully*`});
+    })
+};

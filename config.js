ffdb2ecae1979d0b3fc83a9ec76a11507fdbb1b4
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-24 23:15:09 +0200

    vorterx

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 require("dotenv").config();
 let vorterx = process.env.MODS;
 if (!vorterx) {
-  vorterx = "27686881509","989389383634"; //add your number here 
+  vorterx = "27686881509"; //add your number here 
 }
 
 global.owner = vorterx.split(",");
@@ -11,6 +11,7 @@ global.mongodb = process.env.MONGODB || "none";
 global.mods = process.env.MODS || "27686881509";
 global.botName = process.env.BOTNAME || "Aᴢᴛᴇᴄ-Mᴅ"; // change botName
 global.prefix = process.env.PREFIX || "-";
+global.menu = process.env.MENU || "", //kindly choose the menu style you want [1] default AZTEC-MD menu] [2] SECTOR BOT menu _available as well 
 global.sessionId = process.env.SESSION_ID || "vorterx"; //change session
 global.port = process.env.PORT || 8081;
 

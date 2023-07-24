543dd4cf1f2e9efe5d898d9be7b61018455fdf25
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-25 00:12:59 +0200

    vorterx

--- commands/_status_pf.js
+++ commands/_status_pf.js
@@ -2,12 +2,9 @@ module.exports = {
 name: "status",
 description: "Status",
 category: "Owner",
-  start: async(vorterx,m,{prefix,isCreator,toReact,text}) => {
-if (!isCreator) { await toReact("⛔"); return vorterx.sendMessage(m.from, {text: '*This command is only for my owner*'}, {quoted: m});
+  start: async(vorterx,m,{prefix,iscreator,toReact,text}) => {
+if (!iscreator) { await toReact("⛔"); return vorterx.sendMessage(m.from, {text: '*This command is only for my owner*'}, {quoted: m});
                 }
-  
-if (!text) { await toReact("⛔"); return m.reply("*Provide me with a text ex aztec md*");
-           }
   await toReact("📍");
     await vorterx.updateProfileStatus(text)
     m.reply(`*📍Bio of the bot has been successfully changed*`);

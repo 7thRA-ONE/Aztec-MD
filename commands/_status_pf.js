6f9cb080f23de6303cae6800502d7c70a842f51b
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-25 00:07:19 +0200

    vorterx

--- commands/_status_pf.js
+++ commands/_status_pf.js
@@ -3,7 +3,9 @@ name: "status",
 description: "Status",
 category: "Owner",
   start: async(vorterx,m,{prefix,isCreator,toReact,text}) => {
-
+if (!isCreator) { await toReact("⛔"); return vorterx.sendMessage(m.from, {text: '*This command is only for my owner*'}, {quoted: m});
+                }
+  
 if (!text) { await toReact("⛔"); return m.reply("*Provide me with a text ex aztec md*");
            }
   await toReact("📍");

5338eeeef4b581f744768b7133f613930d5f642b
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-25 06:26:33 +0200

    vorterx

--- commands/_revoke.js
+++ commands/_revoke.js
@@ -4,7 +4,7 @@ module.exports = {
   category: "Group",
   start: async(vorterx,m,{prefix,toReact,isGroup}) => {
 
-  if(!isGroup) { await toReact("⛔"); return vorterx.sendMessage(m.from,{text: '*This command is for Admins only*'}, {quoted:m});
+  if(!m.isGroup) { await toReact("⛔"); return vorterx.sendMessage(m.from,{text: '*This command is for Admins only*'}, {quoted:m});
                }
     await toReact("✅");
     await vorterx.groupRevokeInvite(m.from).then((res) => {

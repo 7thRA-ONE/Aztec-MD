58de9e03cb506462782ea079a8e61bd9dc978a71
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-25 06:37:23 +0200

    vorterx

--- commands/_revoke.js
+++ commands/_revoke.js
@@ -1,11 +1,18 @@
+//© copyright by Diegoson
+//@aztec
+//@vorterx
+
+
 module.exports = {
   name: "revoke",
   description: "To reset group link",
   category: "Group",
-  start: async(vorterx,m,{prefix,toReact,isGroup}) => {
+  start: async(vorterx,m,{prefix,toReact,isGroup,isBotAdmin}) => {
 
   if(!m.isGroup) { await toReact("⛔"); return vorterx.sendMessage(m.from,{text: '*This command is for Admins only*'}, {quoted:m});
                }
+    if(!isBotAdmin) { await toReact("⛔"); return vorterx.sendMessage(m.from, {text: '*I not an admin l cant procedure*'}, {quoted:m});
+                    }
     await toReact("✅");
     await vorterx.groupRevokeInvite(m.from).then((res) => {
             m.reply(`*🔉Group link has been reserted successfully*`);

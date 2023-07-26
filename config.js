150ef7820d2696bfb15feec67b7e494722d62d2a
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-26 19:14:05 +0200

    vorterx

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ if (!vorterx) {
 
 global.owner = vorterx.split(",");
 global.mongodb = process.env.MONGODB || "none";
+levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
 global.mods = process.env.MODS || "27686881509";
 global.botName = process.env.BOTNAME || "Aᴢᴛᴇᴄ-Mᴅ"; // change botName
 global.prefix = process.env.PREFIX || "-";

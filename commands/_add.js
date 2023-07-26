2272c3f153489e7903a89d6bf545ece6a784e940
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-26 13:37:54 +0200

    vorterx

--- commands/_add.js
+++ commands/_add.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "add",
   description: "To add members",
   category: "Group",
-  start: async(vorterx,m,{prefix,toReact,isGroup,isBotAdmin,isAmin}) => {
+  start: async(vorterx,m,{prefix,text,toReact,isGroup,isBotAdmin,isAmin}) => {
     
 if (!m.isGroup) { await toReact("🚫"); return m.reply("*This cmd isfor the group admins only*");
   }

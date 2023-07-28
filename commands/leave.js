ba4d9a894c33d8b2d71f0a39700b899d3491daa2
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-28 16:12:54 +0200

    vorterx

--- commands/leave.js
+++ commands/leave.js
@@ -4,6 +4,6 @@ module.exports = {
   category: "Group",
   start: async(vorterx, m, { isAdmin, isGroup, toReact, isBotAdmin}) => {
 
-  if(m.isGroup) { await toReact("❌"); return m.reply("*🤔Weres you heading cz this is group cmd*");} await toReact("👋"); m.reply("*👋Later mates until we meet again*"); return await vorterx.groupLeave(m.from,{ quoted: m });
+  if(!m.isGroup) { await toReact("❌"); return m.reply("*🤔Weres you heading cz this is group cmd*");} await toReact("👋"); m.reply("*👋Later mates until we meet again*"); return await vorterx.groupLeave(m.from,{ quoted: m });
   }
 };

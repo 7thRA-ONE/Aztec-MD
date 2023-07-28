7e8835d701cc855edbe2555e9fc1548d1ecd39d2
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-28 18:13:17 +0200

    vorterx

--- commands/_device.js
+++ commands/_device.js
@@ -7,15 +7,13 @@ start: async(vorterx,m,{prefix, toReact,text, command}) => {
 const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
 if (isAndroid) { await toReact("😯");  {
   console.log("User is currently using Android");
-} else {
   console.log("User is not using Android");
 }
+               }}
    //-------[For Ios to se if he/she is using !Phone]------
 const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 if (isiOS) {
   console.log("User is using iOS");
 } else {
   console.log("User is not using iOS");
-}
-   }
- };
+};

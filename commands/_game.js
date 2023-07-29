8db82d0907b73ad5d4285a6b1cc84f11bff1713b
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-29 19:41:30 +0200

    vorterx

---
+++ commands/_game.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "spin",
+  description: "Funny game",
+  category: "Games",
+  start: async(vorterx,m,{prefix,toReact,text,args}) => {
+
+  await toReact("🎲");
+let vorterxi = ['⚀','⚁','⚂','⚃','⚄','⚅','⚁','⚁']
+                let aztec = vorterxi[Math.floor(Math.random() * vorterxi.length)]
+            	m.reply(aztec)
+            	}
+};

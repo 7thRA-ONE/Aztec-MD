591ff0ac72743e1da1cf193e4989849bcf8c7379
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-29 15:32:06 +0200

    vorterx

---
+++ commands/_shot_url.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "linkpoi",
+  description: "To shorten the long link",
+  category: "Extra",
+  start: async(vorterx,m,{prefix, command, toReact,text,args}) => {
+
+ const { fetchJson } = require("../lib/module/function.js");
+  if(!text) { await toReact("🛑"); return m.reply("*Provide me with a link to convert");
+            }
+    await toReact("👍");
+  let aztec = await fetchJson(`https://linkpoi.ga/api.php?url=${text}`)
+                vorterx.sendMessage(m.from, { text: aztec.shorturl + `\nNih Bro`}, { quoted: m });
+  }
+            };

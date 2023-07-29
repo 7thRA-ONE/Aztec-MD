c33b92ee8b46141ebda4c0efef74e8d84d90212b
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-29 17:48:46 +0200

    vorterx

---
+++ commands/_mode.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: "public",
+  description: "To change to public or private",
+  category: "owner",
+  start: async(vorterx,m,{prefix,iscreator,toReact,text}) => {
+
+                if (!iscreator) { await toReact("🛑"); return m.reply("*this cmd is for my owner only*");
+                                }
+    await toReact("🔒");
+                vorterx.public = true
+                m.reply('*Bot has been set to public mode⚙️*');
+break
+  case 'private':
+    if(!iscreator) { await toReact("🔒"); return m.reply("*This cmd is for my owner only*");
+                   }
+    await toReact("🔒");
+    vorterx.public = false
+    m.reply("*Bot has been set to private mode*");
+
+  break
+  default;
+  }
+};

8bbb235b17daf04a74a0ed802b8b38ae6fc462f4
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-07-30 11:29:20 +0200

    vorterx

--- commands/aztec.js
+++ commands/aztec.js
@@ -11,7 +11,23 @@ module.exports = {
     await toReact("🛸");
     src = `https://i.ibb.co/6WV7wbc/20230718-084726.png`;
     let vorterxi = await axios.get(`https://api.github.com/repos/Vorterx/Aztec-MD`);
-    let git = ` *乂 A Z T E C  M D- R E P O 乂*\n\n*🌟Total Stars*: ${vorterxi.data.stargazers_count}\n\n*🛸Users*: ${vorterxi.data.forks}\n\n*🌲Update*: ${vorterxi.data.updated_at}\n\n*🌲Repo*: ${vorterxi.data.html_url}\n\n*🌲Whatsapp group*: none\n\n*🌲Owner*: _27686881509_\n\n\n*©AZTEC-MD*`;
-        await vorterx.sendMessage(m.from, {image: {url: src}, caption: git}, {quoted: m});
+    let git = ` *乂 A Z T E C  M D- R E P O 乂*\n\n*🌟Total Stars*: ${vorterxi.data.stargazers_count}\n\n*🛸Users*: ${vorterxi.data.forks}\n\n*🌲Update*: ${vorterxi.data.updated_at}\n\n*🌲Repo*: ${vorterxi.data.html_url}\n\n*🌲Whatsapp group*: none\n\n*🌲Owner*: _27686881509_`
+    let buttonMessage = {
+            image: {url: src},
+            caption: git,
+            footer: 'vorterx team',
+            headerType: 4,
+            contextInfo: {
+                externalAdReply: {
+                    title: "aztec github",
+                    body: "multi bot v",
+                    thumbnail: pp,
+                    mediaType: 4,
+                    mediaUrl: 'wa.me/27686881509',
+                    sourceUrl: `https://vorterx.com/`,
+                },
+            },
+        };
+        await vorterx.sendMessage(m.from, buttonMessage, {quoted: m});
   }
 };
